343208eaeef2b2cb22cdce49c69141f988e6df8b
mustafaag <mustafaaga23@gmail.com>
2020-01-16 15:33:49 +0100

    Dom events

--- src/App.js
+++ src/App.js
@@ -7,12 +7,23 @@ export default class  App extends React.Component {
     name: 'Goku',
     age: 15
   };
-
+  eventHandler(e){
+    console.log(e.target);
+  }
+  handleMouseOver(e){
+    console.log(e.target,  e.pageX);
+  }
+  handleCopy(e){
+    console.log("original", e);
+  }
   render(){
     return(
       <div className="App">
         <h1>Hello bisha!</h1>
         <p>My name is {this.state.name} and I am {this.state.age} old</p>   
+        <button onClick={this.eventHandler}> Click me!</button>
+        <button onMouseOver={this.handleMouseOver}> Hover me!</button>
+        <p onCopy={this.handleCopy}>What we think, we do!</p>
       </div>
     )
   } 
