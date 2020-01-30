9ffdb7699b874f995702c11b0bdba99e522664c6
mustafaag <mustafaaga23@gmail.com>
2020-01-30 10:08:46 +0100

    #15 - Stateless Components

--- src/content.js
+++ src/content.js
@@ -1,24 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Content extends Component {
-  render () {
-    const { people } = this.props;
-    const peopleList = people.map(person => {
-      return (
-        <div key={person.id}>
-          <div> Name: {person.name}</div>
-          <div> Age: {person.age}</div>
-          <div> Belt {person.belt} </div>
-        </div>
-      )
-    })
+const Content = ({ people }) => {
+  const peopleList = people.map(person => {
     return (
-      <div className='person-list'>
-        {peopleList}
+      <div key={person.id}>
+        <div> Name: {person.name}</div>
+        <div> Age: {person.age}</div>
+        <div> Belt {person.belt} </div>
       </div>
-
     )
-  }
+  });
+  return (
+    <div className='person-list'>
+      {peopleList}
+    </div>
+  );
 }
 
 export default Content
