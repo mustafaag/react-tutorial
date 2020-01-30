805f248efac15eccff2c8f06648cddd2d782a01a
mustafaag <mustafaaga23@gmail.com>
2020-01-30 10:23:57 +0100

    Conditional output

--- src/content.js
+++ src/content.js
@@ -2,19 +2,41 @@ import React from 'react'
 
 const Content = ({ people }) => {
   const peopleList = people.map(person => {
-    return (
+    // Conditial output IF
+    if (person.age >= 18) {
+      return (
+        <div key={person.id}>
+          <div> Name: {person.name}</div>
+          <div> Age: {person.age}</div>
+          <div> Belt {person.belt} </div>
+        </div>
+      )
+    } else {
+      return null;
+    }
+  });
+    // Ternary operation 
+  const peopleList2 = people.map(person => {
+    return person.age >= 18 ? (
       <div key={person.id}>
         <div> Name: {person.name}</div>
         <div> Age: {person.age}</div>
         <div> Belt {person.belt} </div>
-      </div>
-    )
+      </div>) : null
   });
   return (
     <div className='person-list'>
-      {peopleList}
+      { people.map(person => {
+        return person.age >= 18 ? (
+          <div key={person.id}>
+            <div> Name: {person.name}</div>
+            <div> Age: {person.age}</div>
+            <div> Belt {person.belt} </div>
+          </div>) : null
+      })
+      }
     </div>
-  );
+  )
 }
 
 export default Content
